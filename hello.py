1bfd838a214ace407657b0e8e87efbea710a4823
Malik Shahzad <malikshahzad4726@gmail.com>
2026-03-25 13:35:11 +0400

    Add hello.py with a simple greeting and date display

---
+++ hello.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+
+def main():
+    print("Malik Shahzad")
+
+    today_date = date.today()
+    print(f"Today's date is {today_date}.")
+
+    number_of_days_coding = 1
+    print(f"I have been coding for {number_of_days_coding} day(s).")
+
+
+if __name__ == "__main__":
+    main()
